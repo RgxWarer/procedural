8944a0073ce74ab56b1c0761163c1d56ae99a8e0
irishakeks <irishakeks@mail.ru>
2019-04-02 14:34:05 +0700

    7а

--- client.py
+++ client.py
@@ -3,15 +3,16 @@ import logic
 
 
 def main():
-    if len(sys.argv) != 3:
-        return print("Вы не ввели нужные аргументы.")
+    # if len(sys.argv) != 3:
+    #     return print("Вы не ввели нужные аргументы.")
 
-    input_name = sys.argv[1]
-    output_name = sys.argv[2]
+    input_name = "input.txt"#sys.argv[1]
+    output_name = "output.txt"#sys.argv[2]
 
     logic.input_shapes(input_name)
 
-    logic.output_shapes(output_name)
+    # logic.output_shapes(output_name)
+    logic.multiMethod(output_name)
 
 
 if __name__ == '__main__':
--- logic.py
+++ logic.py
@@ -1,5 +1,5 @@
 shapes_list = []
-# parr_keys = ['height', 'width', 'length', 'density']
+parr_keys = ['height', 'width', 'length', 'density']
 sphere_keys = ['radius', 'density']
 
 
@@ -46,18 +46,53 @@ def output_shapes(file_name):
 
     while len(shapes_list) != 0:
         shape = shapes_list.pop()
-        if list(shape.keys()) == sphere_keys:
-            output_file.write(str(count) + ": It's sphere: "
-                                           "r = " + shape['radius'] + ", "
-                                            "d = " + shape['density']  + "\n")
-            count += 1
-        else:
-            output_file.write(str(count) + ": It's parallelepiped: "
-                                           "h = " + shape['height'] + ", "
-                                            "w = " + shape['width'] + ", "
-                                            "l = " + shape['length'] + ", "
-                                              "d = " + shape['density'] + "\n")
-            count += 1
+        output_shape(shape, count, output_file)
+        count += 1
 
     output_file.write("\nEmpty container\n"
                       "Container contains " + str(len(shapes_list)))
+
+
+def output_shape(shape, count, output_file):
+    if list(shape.keys()) == sphere_keys:
+        output_file.write(str(count) + ": It's sphere: "
+                                       "r = " + shape['radius'] + ", "
+                                                                  "d = " + shape['density'] + "\n")
+        count += 1
+    else:
+        output_file.write(str(count) + ": It's parallelepiped: "
+                                       "h = " + shape['height'] + ", "
+                                                                  "w = " + shape['width'] + ", "
+                                                                                            "l = " + shape[
+                              'length'] + ", "
+                                          "d = " + shape['density'] + "\n")
+
+
+def multiMethod(file_name):
+    with open(file_name, 'w') as out:
+        out.write("Multimethod\n")
+
+        for i in range(len(shapes_list) - 1):
+            j = i + 1
+            while j < len(shapes_list):
+                if list(shapes_list[i].keys()) == sphere_keys:
+                    if list(shapes_list[j].keys()) == sphere_keys:
+                        out.write("Sphere and sphere\n")
+                    elif list(shapes_list[j].keys()) == parr_keys:
+                        out.write("Sphere and parr\n")
+                    else:
+                        out.write("Unknown type\n")
+                elif list(shapes_list[i].keys()) == parr_keys:
+                    if list(shapes_list[j].keys()) == sphere_keys:
+                        out.write("Parr and sphere.\n")
+                    elif list(shapes_list[j].keys()) == parr_keys:
+                        out.write("Parr and parr\n")
+                    else:
+                        out.write("Unknown type\n")
+                else:
+                    out.write("Unknown type\n")
+                output_shape(shapes_list[i], i, out)
+                output_shape(shapes_list[j], j, out)
+                j += 1
+
+        out.close()
